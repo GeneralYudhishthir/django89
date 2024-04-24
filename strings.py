19ad87ce8f33c0dc2f2ae75c9d6b1ac4d15e7a3c
GeneralYudhishthir <barse.dharmaraj@gmail.com>
2024-04-24 20:46:49 +0530

    added new files

---
+++ strings.py
@@ -0,0 +1,15 @@
+print('hello world')
+print("hello world")
+print('''hello world''')
+print("""hello world""")
+
+print("Hello, my name is Dhiraj!")
+
+
+print(123)
+
+print([1,2,3,4,'Dhiraj'])
+
+print({1,2,3,4,5,5})
+
+print({1:2,2:3,3:4})
